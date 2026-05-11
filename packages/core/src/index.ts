422460eb02e800458af6c49b286fad9ad6ceadc2
visi6630 <evisbakiu@evisbakiu.com>
2026-05-11 17:56:53 +0200

    Add supabase connection

--- packages/core/src/index.ts
+++ packages/core/src/index.ts
@@ -1,3 +1,4 @@
 export * from "./types/index.js";
 export * from "./exercises/diktat.js";
 export { diktatLessons, sampleDiktatLesson } from "./content/diktat-lessons.js";
+export { createSupabaseClient } from "./lib/supabase.js";
